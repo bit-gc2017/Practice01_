a293fef43e9d756d76df83dd4600f0c467594a1d
remys <remys@remys-notebook>
2017-07-11 18:50:23 +0900

    연습문제01 풀이

---
+++ src/com/javaex/practice/Problem01.java
@@ -0,0 +1,16 @@
+package com.javaex.practice;
+
+public class Problem01 {
+
+	public static void main(String[] args) {
+		int i;
+		
+		for(i=1; i<=100; i++){
+			if(i%5==0 && i%7==0){
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
---
+++ src/com/javaex/practice/Problem05.java
@@ -0,0 +1,17 @@
+package com.javaex.practice;
+
+public class Problem05 {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i<=10 ; i++){
+			
+			for(int j=i; j<i+10 ; j++){
+				System.out.print(j + "\t");
+			}	
+			System.out.println("");
+		}
+		
+	}
+
+}
